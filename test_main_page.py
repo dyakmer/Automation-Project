c81b3b434cdea28d405e12dd2aca669993c06cd1
Dyakmer_Work <nickdyakov@gmail.com>
2023-08-03 15:34:44 +0300

    Adaptation to Page object

--- test_main_page.py
+++ test_main_page.py
@@ -1,7 +1,13 @@
 from selenium.webdriver.common.by import By
 
-def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    browser.get(link)
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+
+
+def go_to_login_page(browser): #переход на страницу входа в систему
     login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
     login_link.click()
+
+
+def test_guest_can_go_to_login_page(browser): #тестовый гость переходит на страницу входа в систему
+    browser.get(link)
+    go_to_login_page(browser)
