125f54c5d00140ead3d55b8eeb924eb14c91ad2b
Dyakmer_Work <nickdyakov@gmail.com>
2023-08-15 11:21:59 +0300

    Adding transitions between pages

--- pages/locators.py
+++ pages/locators.py
@@ -3,3 +3,16 @@ from selenium.webdriver.common.by import By
 
 class MainPageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+
+class LoginPageLocators():
+    LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
+    REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
+    LOGIN_URL_SUBSTRING = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"  # строка, которая должна содержаться в URL страницы
+
+    # Новые селекторы для форм регистрации и логина
+    LOGIN_EMAIL = (By.NAME, "login-username")
+    LOGIN_PASSWORD = (By.NAME, "login-password")
+    REGISTER_EMAIL = (By.NAME, "registration-email")
+    REGISTER_PASSWORD = (By.NAME, "registration-password1")
+    REGISTER_CONFIRM_PASSWORD = (By.NAME, "registration-password2")
+    REGISTER_BUTTON = (By.NAME, "registration_submit")
--- pages/main_page.py
+++ pages/main_page.py
@@ -2,12 +2,13 @@ from .base_page import BasePage
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException
 from .locators import MainPageLocators
+from .login_page import LoginPage
 
 
 class MainPage(BasePage):
     def go_to_login_page(self):
-        login_link = self.browser.find_element(By.CSS_SELECTOR, "#login_link")
-        login_link.click()
+        link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
+        link.click()
 
     def should_be_login_link(self):
         assert self.is_element_present(*MainPageLocators.LOGIN_LINK), "Login link is not presented"
@@ -18,3 +19,11 @@ class MainPage(BasePage):
         except NoSuchElementException:
             return False
         return True
+
+    def test_guest_can_go_to_login_page(browser):
+        link = "http://selenium1py.pythonanywhere.com"
+        page = MainPage(browser, link)
+        page.open()
+        page.go_to_login_page()
+        login_page = LoginPage(browser, browser.current_url)
+        login_page.should_be_login_page()
